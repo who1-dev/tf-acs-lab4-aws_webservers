d9ac3ed3c848cfe908c32f234e8f9bacad6fd860
who1-dev <caranayjohn21.student@gmail.com>
2024-10-27 21:34:16 -0400

    Added default value for common variables

--- variables.tf
+++ variables.tf
@@ -3,12 +3,24 @@ variable "env" {
   description = "Deployment Environment"
 }
 
+variable "region" {
+  default     = "us-east-1"
+  type        = string
+  description = "Region to be deployed"
+}
+
+# Default tags
 variable "default_tags" {
+  default = {
+    "Owner" = "jcaranay",
+    "App"   = "Web"
+  }
   type        = map(any)
   description = "Default tags to be appliad to all AWS resources"
 }
 
 variable "namespace" {
+  default = "LAB4"
   type        = string
   description = "Namespace"
 }
